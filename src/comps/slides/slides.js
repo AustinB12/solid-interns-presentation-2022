a96ab8edca7eb2f4d366da89c951a1db9e398431
Austin Baird <42442591+AustinB12@users.noreply.github.com>
2022-06-13 22:59:37 -0400

    Started Slides, Gist Examples

--- src/comps/slides/slides.js
+++ src/comps/slides/slides.js
@@ -2,6 +2,9 @@ import React, { useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { Slide } from "../slide/slide";
 
+import wiresImage from "../../assets/cleanVsClutter.png";
+import SRPExample from "../../assets/srp.jpeg";
+
 const Slides = ({ currentPosition, setNumOfSlides }) => {
   let theSlides = [
     <motion.div
@@ -11,7 +14,16 @@ const Slides = ({ currentPosition, setNumOfSlides }) => {
       transition={{ x: { duration: 0.5 } }}
     >
       <Slide>
-        <h1>SOLID Programmming Principles</h1>
+        <div className="pageBreak">
+          <h1>SOLID Programmming Principles</h1>
+          <p>What are they?</p>
+        </div>
+        <img
+          src={wiresImage}
+          alt="Clean vs Cluttered wires"
+          width={"40%"}
+          height={"40%"}
+        />
       </Slide>
     </motion.div>,
     <motion.div
@@ -21,7 +33,19 @@ const Slides = ({ currentPosition, setNumOfSlides }) => {
       transition={{ x: { duration: 0.5 } }}
     >
       <Slide>
-        <h1 style={{ color: "yellow" }}>Slide 2</h1>
+        <h1>Let's Break It Down</h1>
+        <h2>S-O-L-I-D</h2>
+        <img
+          src="https://www.freecodecamp.org/news/content/images/2020/08/solid.png"
+          alt="Solid Principles"
+        />
+        <ul style={{ listStyle: "none", fontSize: "20px" }}>
+          <li>Single Responsibility Principle</li>
+          <li>Open/Closed Principle</li>
+          <li>Liskov Substitution Principle</li>
+          <li>Interface Segregation Principle</li>
+          <li>Dependency Inversion Principle</li>
+        </ul>
       </Slide>
     </motion.div>,
     <motion.div
@@ -31,7 +55,8 @@ const Slides = ({ currentPosition, setNumOfSlides }) => {
       transition={{ x: { duration: 0.5 } }}
     >
       <Slide>
-        <h1 style={{ color: "red" }}>Slide 3</h1>
+        <h1>Single Responsibility Principle</h1>
+        <img src={SRPExample} alt="Fork and Spoon VS a Spork" />
       </Slide>
     </motion.div>,
     <motion.div
